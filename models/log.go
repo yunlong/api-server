9e2e2a8ff9348576bc32171cbb73ad540533aff7
chanhlv93 <thanhniendongvan@gmail.com>
2016-11-30 13:28:20 +0700

    release code api-server

--- models/log.go
+++ models/log.go
@@ -4,7 +4,7 @@ import "time"
 
 type Log struct {
 	ID                   int	`sql:"AUTO_INCREMENT" json:"id"`
-	DeviceId 	     int	`json:"deviceId"`
+	DeviceId 	     	 int	`json:"deviceId"`
 	Status               string	`json:"status"`
 	Name                 string	`json:"name"`
 	Version              string	`json:"version"`
